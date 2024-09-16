bcaf27e746dcfbbc5dc41ccf365d87acd94800db
Your Name <you@example.com>
2024-09-16 09:31:33 -0500

    se crea proyecto back

---
+++ src/main/java/com/example/SURATECH5/Suratech5Application.java
@@ -0,0 +1,13 @@
+package com.example.SURATECH5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Suratech5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Suratech5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SURATECH5/Suratech5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SURATECH5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Suratech5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
